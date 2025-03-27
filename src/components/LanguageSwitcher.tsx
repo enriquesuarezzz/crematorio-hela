c68a79a08c9df28a544f731797d45fbd73eac971
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-03-27 12:12:54 +0000

    LanguageSwitcherComponentUpdate

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,8 +1,8 @@
 // src/LanguageSwitcher.tsx
 import React from "react";
 import { useTranslation } from "react-i18next";
-import Spanish from "./svg/spanish";
 import English from "./svg/english";
+import Spanish from "./svg/spanish";
 
 const LanguageSwitcher: React.FC = () => {
   const { i18n } = useTranslation();
